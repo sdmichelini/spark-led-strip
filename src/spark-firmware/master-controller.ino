c6f41acb9990a3af9f6deb11a7e1360fc181471a
Steve Michelini <sdmichelini@gmail.com>
2014-02-26 17:07:03 -0500

    Updated to Allow Web Server to Get Time

--- src/spark-firmware/master-controller.ino
+++ src/spark-firmware/master-controller.ino
@@ -28,137 +28,120 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 
-// This #include statement was automatically added by the Spark IDE.
-//Spark Time library by Brian Ogilvie           
-//Can be found at https://github.com/bkobkobko/SparkCoreLibraries
-#include "SparkTime.h"
-
-//We need time for when we enter auto mode
-UDP UDPClient;
-SparkTime rtc;
-
-unsigned long currentTime;
-//Default Command
+//Command Variables
 String _command="raffff00";
-String _lastCommand="";
 String _lastNotifiedCommand="raffff00";
-
+//Loop Count and Notification Variables
 unsigned long _loopCount=0;
 unsigned long _lastLoop=0;
 
+//Color Constants
+const String NIGHT_COLOR ="rcFF7800";
+const String DIM_NIGHT_COLOR = "rc0F0A00";
+const String DAY_COLOR ="rcFFE9C8";
+const String OFF_COLOR ="rc000000";
+const String NOTIFICATION_COLOR ="rbffffff";
+
 
 void setup() {
-    //Connecting to the Clock
-    rtc.begin(&UDPClient, "north-america.pool.ntp.org");
-    rtc.setTimeZone(-5); // gmt offset
     Spark.function("setV",setValue);
-
-    //join i2c as master
     Wire.begin();                
-    
     Serial.begin(9600);
-    pinMode(D2,OUTPUT);
-    
 }
 
-
-//simply check if a value is between two numbers
 bool inRange(uint8_t val, uint8_t low, uint8_t high){
     return ((val<=high)&&(low<=val));
 }
 
-
-
-
 void loop(){
-   digitalWrite(D2,HIGH);
-   //If we have notified for more than a loop go back to last color
-   if(_command=="notify"){
+    if(_command=="notify"){
        if(_loopCount>(_lastLoop+1)){
            _command=_lastNotifiedCommand;
            setValue(_command);
        }
-   }
+    }
    
-   Serial.println("Current Color Command:"+_command);
-   _loopCount++;
+    Serial.println("Current Color Command: "+_command);
+    _loopCount++;
     delay(1000);
 }
+//Wrapper for i2c functionality
+bool i2c_write(String send){
+    Wire.beginTransmission(4); 
+    Wire.write(send.c_str());         
+    Wire.endTransmission(true);    
+}
 
+//Handle Automatic Functions
+//Returns true if a valid auto command is recognized
+bool handleAutoCommand(String Command){
+    if(_command=="auto")
+    {
+        if(Command=="sunrise"){
+            i2c_write(DAY_COLOR);
+            Serial.println("Auto: Sunrise");
+            return true;
+        }
+        else if(Command=="sunset"){
+            i2c_write(NIGHT_COLOR);
+            Serial.println("Auto: Sunset");
+            return true;
+        }
+        else if(Command=="off"){
+            i2c_write(OFF_COLOR);
+            Serial.println("Auto: Off");
+            return true;
+        }
+        else if(Command=="dim"){
+            i2c_write(DIM_NIGHT_COLOR);
+            Serial.println("Auto: Dim Nigh Color");
+            return true;
+        }
+        return false;
+    }
+    return false;
+}
 
 //Let Spark Decide When to Set LED's when command is auto
 int setValue(String Command){
-    //If we are currently in auto state
-    if(_command=="auto"){
-        if(Command=="sunrise"){
-            String send = "rcFFE9C8";
-            Wire.beginTransmission(4); // transmit to device #4
-            Wire.write(send.c_str());         
-            Wire.endTransmission(true);    // stop transmitting
-            Serial.println("Auto: Sunrise");
-            return 0;
-        }
-            
-        else if(Command=="sunset"){
-            String send = "rcFF7800";
-            Wire.beginTransmission(4); // transmit to device #4
-            Wire.write(send.c_str());         
-            Wire.endTransmission(true);    // stop transmitting
-            Serial.println("Auto: Sunset");
-            return 0;
-        }
-        else if(Command=="off"){
-            String send = "rc000000";
-            Wire.beginTransmission(4); // transmit to device #4
-            Wire.write(send.c_str());         
-            Wire.endTransmission(true);    // stop transmitting
-            Serial.println("Auto: Off");
-            return 0;
-        }
-    }
-    _command=Command;
     Serial.println("Recieved Command: "+Command);
+    if(handleAutoCommand(Command))return 0;
+    //Wait til after to reset command because the command could be an auto command if we are in the auto state
+    _command=Command;
+    
     if(_command=="auto")
     {
-        currentTime=rtc.now();
-        //Sunset
-        if(inRange(rtc.hour(currentTime),17,24)||inRange(rtc.hour(currentTime),0,7)){
-            String send = "rcFF7800";
-            Wire.beginTransmission(4); // transmit to device #4
-            Wire.write(send.c_str());         
-            Wire.endTransmission(true);    // stop transmitting
-            Serial.println("Auto: Sunset");
-            _lastNotifiedCommand="auto";
-        }
-        //Sunrise
-        else{
-            String send = "rcFFE9C8";
-            Wire.beginTransmission(4); // transmit to device #4
-            Wire.write(send.c_str());          
-            Wire.endTransmission(true);    // stop transmitting
-            Serial.println("Auto: Sunrise");
-            _lastNotifiedCommand="auto";
-        }
+        i2c_write(DAY_COLOR);
         _lastNotifiedCommand="auto";
-        _lastCommand="";
-        
+    }
+    //Set it to Auto but Start in Off Position
+    else if(_command=="auto%20off")
+    {
+        i2c_write(OFF_COLOR);
+        Serial.println("Auto Off");
+        _lastNotifiedCommand="auto";
+        _command="auto";
+        return 0;
+    }
+    //Python Detected Sunsets
+    else if(_command=="autos")
+    {
+        i2c_write(NIGHT_COLOR);
+        Serial.println("Auto Off");
+        _lastNotifiedCommand="auto";
+        _command="auto";
+        return 0;
     }
     else if(_command=="notify")
     {
-        String send="rbffffff";
-        Wire.beginTransmission(4); // transmit to device #4
-        Wire.write(send.c_str());        
-        Wire.endTransmission(true);    // stop transmitting
+        String send=NOTIFICATION_COLOR;
+        i2c_write(send); 
         _lastLoop=_loopCount;
     }
     else
     {
-        
-        Wire.beginTransmission(4); // transmit to device #4
-        Wire.write(_command.c_str());         
-        Wire.endTransmission(true);    // stop transmitting
+        i2c_write(_command);
         _lastNotifiedCommand=_command;
     }
     return 0;
 }
-
